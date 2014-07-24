c9a5c838687a770a81004b694072cec7472ac096
Digerati <digerati.working@gmail.com>
2014-07-23 23:18:45 -0700

    created functions

--- cachematrix.R
+++ cachematrix.R
@@ -10,26 +10,31 @@ makeCacheMatrix <- function(x = matrix()) {
         x<<-y
         m<<-NULL
     }
-    get<-function(x) 
+    get<-function() x 
     setmatrix<-function(solve) m<<- solve
     getmatrix<-function() m
-    list(set=set, get=get,
-         setmatrix=setmatrix,
-         getmatrix=getmatrix)
+    list(set=set, get=get,setmatrix=setmatrix,getmatrix=getmatrix)
 }
 
-#This function computes the inverse of the matrix returned by makeCacheMatrix.  
+#Computes the inverse of the matrix returned by makeCacheMatrix.  
 #If the inverse has already been calculated (and the matrix has not changed), 
 #then cacheSolve retrieves the inverse from the cache.
 
+
+
 cacheSolve <- function(x, ...) {
+    
     m<-x$getmatrix()
     if(!is.null(m)){
         message("getting cached data")
         return(m)
     } else {
-    m<-solve(x$get())
+       
+#Calculate the inverse 
+
+    m <- solve(x$get(), ...)
     x$setmatrix(m)
     m
-    }
+}   
 }
+
